cc9b5fbe53193895ab7fc40a96d1de20dbbcbb26
Peter Ingles <peter.ingles@gmail.com>
2023-07-14 13:31:30 -0500

    Added addIce and addFire functions and handlers

--- script.js
+++ script.js
@@ -2,4 +2,24 @@ $(document).ready(onReady);
 
 function onReady() {
   console.log('Hey jQuery!');
+  $('#fireButton').on('click', addFire )
+  $('#iceButton').on('click', addIce )
+}
+
+let fireEmoji ='🔥' 
+let iceEmoji = '❄️'
+
+function addFire(){
+  console.log(fireEmoji)
+  $('#magicalDiv').append(` 
+  <span> ${fireEmoji} </span>`);
+  // Potential alternative 👇
+  // // $('#magicalDiv').append( `  
+  // <span class="fireEmoji">🔥</span>`);
+}
+
+function addIce(){
+  console.log(iceEmoji)
+  $('#magicalDiv').append(` 
+  <span> ${iceEmoji} </span>`);
 }
