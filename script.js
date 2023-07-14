7f6899ec38a6a3dece48c80a69a03c5b8c15df5c
Peter Ingles <peter.ingles@gmail.com>
2023-07-14 14:52:25 -0500

    Finsihed part 2 and 3

--- script.js
+++ script.js
@@ -2,14 +2,16 @@ $(document).ready(onReady);
 
 function onReady() {
   console.log('Hey jQuery!');
-  $('#fireButton').on('click', addFire )
-  $('#iceButton').on('click', addIce )
+  $('#fireButton').on('click', addFire)
+  $('#iceButton').on('click', addIce)
+  $('.submit-button').on('click', handleSubmit)
+  $('#tableAffirm').on('click', '#affirmRow', deleteRow)
 }
 
-let fireEmoji ='🔥' 
+let fireEmoji = '🔥'
 let iceEmoji = '❄️'
 
-function addFire(){
+function addFire() {
   console.log(fireEmoji)
   $('#magicalDiv').append(` 
   <span> ${fireEmoji} </span>`);
@@ -18,8 +20,29 @@ function addFire(){
   // <span class="fireEmoji">🔥</span>`);
 }
 
-function addIce(){
+function addIce() {
   console.log(iceEmoji)
   $('#magicalDiv').append(` 
   <span> ${iceEmoji} </span>`);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  console.log('in handleSubmit')
+  const affImput = $("#affirmInput").val();
+  const authorImput = $("#authorInput").val();
+
+  console.log('toDoText', affImput)
+  $("#tableAffirm").append(`
+  <tr id= "affirmRow">
+    <td>${affImput}</td>
+    <td>${authorImput}</td>
+    <td><button>❌</button</td>
+  </tr> `)
+}
+
+function deleteRow(){
+  console.log('inside deleteRow', $(this))
+  $(this).remove()
+
 }
